8221c566e49d08a0df2cf07878b75743da75bdcd
Paul Horwitz <phorwitz@concord.org>
2019-09-20 16:16:11 -0400

    Edited styling of graph title

--- graphs.js
+++ graphs.js
@@ -36,11 +36,11 @@ function makeGraph() {
             }
             myReversals = countReversals(myValues);
             if (myReversals == 0) {
-                revStr = "No reversals."
+                revStr = '<span style="color:red">No reversals</span>';
             } else if (myReversals == 1) {
-                revStr = "1 reversal."
+                revStr = '<span style="color:red">One reversal</span>';
             } else {
-                revStr = myReversals + " reversals."
+                revStr = '<span style="color:red">' + myReversals + ' reversals</span>';
             }
             var trace = {
                 type: 'scatter',
@@ -106,14 +106,16 @@ function makeGraph() {
 }
 
 //Count the number of times the graph reverses direction
+/*
+Note: this algorithm is too simple. It looks for significant differences between neighboring values in order to find extrema, but this means it won't count shallow but wide valleys or mountains. We need to be more clever, looking ahead and behind to see changes beyond the immediate neighborhood.
+*/
+
 function countReversals(values) {
     var tolerance = .1 //Values that differ by plus or minus this number will be counted as equal.
     var count = 0,
-        first,
-        second,
-        thisVal,
-        after,
-        fifth;
+        before,
+        now,
+        after;
     if (values.length > 5) {
         for (var i = 1; i < values.length - 1; i++) {
             before = values[i - 1];
@@ -137,19 +139,19 @@ function approxEqual(value1, value2, tolerance) {
 
 function reportConceptData() {
     var ci = new Object();
-    ci["LG1.A3"] = "Sex determination. Females have two X chromosomes. Males have one X and one Y.";
-    ci["LG1.C2a"] = "Simple dominance. Only one dominant allele is needed to produce the dominant trait.";
-    ci["LG1.C2b"] = "Recessive traits. For most traits, the absence is recessive.";
-    ci["LG1.C2c"] = "X linkage. Males receive only one copy of the X chromosome and pass on their X chromosome only to their daughters.";
-    ci["LG1.C2d"] = "Polyallelic. Some traits are controled by multiple alleles that can form a ranked series in terms of dominance.";
-    ci["LG1.C3"] = "Incomplete dominance. For some traits both alleles will have some effect with neither being completely dominant.";
-    ci["LG1.P1"] = "Genotype-to-phenotype mapping. Given a genotype, predict the phenotype of an organism.";
-    ci["LG1.P2"] = "Phenotype-to-genotype mapping. Given a phenotype, determine the possible genotypes of an organism.";
-    ci["LG1.C4a"] = "Epistasis. A gene can mask the effect of other genes.";
-    ci["LG2.P1"] = "Gamete selection. Create or select parental gametes to create an individual offspring with a specific phenotype.";
-    ci["LG3.P1"] = "Parent genotypes. Set or select parental genotypes to produce a specific pattern of traits in offspring.";
-    ci["LG3.P3"] = "Patterns in offspring. Use patterns in the phenotypes of a group of offspring to predict the genotypes of the parents.";
-    ci["LG3.P4"] = "Test cross. Breed with a recessive organism to determine an unknown genotype.";
+    ci["LG1.A3"] = "Sex determination.<br>Females have two X chromosomes. Males have one X and one Y.";
+    ci["LG1.C2a"] = "Simple dominance.<br>Only one dominant allele is needed to produce the dominant trait.";
+    ci["LG1.C2b"] = "Recessive traits.<br>For most traits, the absence is recessive.";
+    ci["LG1.C2c"] = "X linkage.<br>Males receive only one copy of the X chromosome and pass on their X chromosome only to their daughters.";
+    ci["LG1.C2d"] = "Polyallelic.<br>Some traits are controled by multiple alleles that can form a ranked series in terms of dominance.";
+    ci["LG1.C3"] = "Incomplete dominance.<br>For some traits both alleles will have some effect with neither being completely dominant.";
+    ci["LG1.P1"] = "Genotype-to-phenotype mapping.<br>Given a genotype, predict the phenotype of an organism.";
+    ci["LG1.P2"] = "Phenotype-to-genotype mapping.<br>Given a phenotype, determine the possible genotypes of an organism.";
+    ci["LG1.C4a"] = "Epistasis.<br>A gene can mask the effect of other genes.";
+    ci["LG2.P1"] = "Gamete selection.<br>Create or select parental gametes to create an individual offspring with a specific phenotype.";
+    ci["LG3.P1"] = "Parent genotypes.<br>Set or select parental genotypes to produce a specific pattern of traits in offspring.";
+    ci["LG3.P3"] = "Patterns in offspring.<br>Use patterns in the phenotypes of a group of offspring to predict the genotypes of the parents.";
+    ci["LG3.P4"] = "Test cross.<br>Breed with a recessive organism to determine an unknown genotype.";
     ci["LG99.A"] = "Some allele changes don't affect phenotype.";
     return ci;
 }
