658200c850291ec460b0acc312e887503e99571e
Paul Horwitz <phorwitz@concord.org>
2019-10-31 14:44:15 -0400

    Added scanning across challenges for individual students.

--- sequences.js
+++ sequences.js
@@ -36,8 +36,6 @@ function findFutureProbs() { //Goes through all the actions for every student, s
     }
 }
 
-
-
 function downloadTable(table) {
     var hintsTable = document.getElementById("hintsTable");
     var hintsCSV = tableToCSV(hintsTable);
@@ -46,66 +44,8 @@ function downloadTable(table) {
 }
 
 
-function makeProfile() {
-    var activitiesArray = ["allele-targetMatch-visible-simpleDom", "allele-targetMatch-visible-simpleDom2", "allele-targetMatch-hidden-simpleDom", "allele-targetMatch-hidden-simpleDom2",
-        "allele-targetMatch-visible-armorHorns",
-        "allele-targetMatch-visible-armorHorns2",
-        "allele-targetMatch-visible-armorHorns3",
-        "allele-targetMatch-hidden-armorHorns",
-        "allele-targetMatch-hidden-armorHorns2",
-        "allele-targetMatch-hidden-armorHorns3",
-        "allele-targetMatch-visible-simpleColors",
-        "allele-targetMatch-visible-simpleColors2",
-        "allele-targetMatch-visible-simpleColors3",
-        "allele-targetMatch-visible-simpleColors4",
-        "allele-targetMatch-visible-simpleColors5",
-        "allele-targetMatch-hidden-simpleColors",
-        "allele-targetMatch-hidden-simpleColors2",
-        "allele-targetMatch-hidden-simpleColors3",
-        "allele-targetMatch-visible-harderTraits",
-        "allele-targetMatch-visible-harderTraits2",
-        "allele-targetMatch-hidden-harderTraits",
-        "allele-targetMatch-hidden-harderTraits2"
-    ];
-    var activity,
-        student,
-        clas,
-        profile;
-    for (var i = 0; i < students.length; i++) {
-        student = students[i];
-        clas = student.class;
-        console.log("Class " + clas.id + ", student " + student.id);
-        for (var j = 0; j < activitiesArray.length; j++) {
-            activity = activitiesArray[j];
-            var arr = filterActions(student, activity);
-            var profile = aggregateActions(arr);
-            console.log("Activity " + activity);
-            console.log(profile);
-        }
-    }
-}
 
 function makeIndividualProfile(studentId, activityName) {
-    var activitiesArray = ["allele-targetMatch-visible-simpleDom", "allele-targetMatch-visible-simpleDom2", "allele-targetMatch-hidden-simpleDom", "allele-targetMatch-hidden-simpleDom2",
-        "allele-targetMatch-visible-armorHorns",
-        "allele-targetMatch-visible-armorHorns2",
-        "allele-targetMatch-visible-armorHorns3",
-        "allele-targetMatch-hidden-armorHorns",
-        "allele-targetMatch-hidden-armorHorns2",
-        "allele-targetMatch-hidden-armorHorns3",
-        "allele-targetMatch-visible-simpleColors",
-        "allele-targetMatch-visible-simpleColors2",
-        "allele-targetMatch-visible-simpleColors3",
-        "allele-targetMatch-visible-simpleColors4",
-        "allele-targetMatch-visible-simpleColors5",
-        "allele-targetMatch-hidden-simpleColors",
-        "allele-targetMatch-hidden-simpleColors2",
-        "allele-targetMatch-hidden-simpleColors3",
-        "allele-targetMatch-visible-harderTraits",
-        "allele-targetMatch-visible-harderTraits2",
-        "allele-targetMatch-hidden-harderTraits",
-        "allele-targetMatch-hidden-harderTraits2"
-    ];
     var student = findStudent(studentId),
         clas = student.class,
         arr,
@@ -117,15 +57,16 @@ function makeIndividualProfile(studentId, activityName) {
 }
 
 //Returns an array of all the actions <student> took while engaged in <activity></activity>
-function filterActions(student, activity) {
+function filterActions(studentId, activity) {
     var arr = [],
-        act;
+        act,
+        student = findStudent(studentId);
     if (student.actions) {
         for (var i = 0; i < student.actions.length; i++) {
             if (student.actions[i].activity === activity) {
                 act = student.actions[i];
                 arr.push(student.actions[i]);
-                //          console.log(act.time + ": " + act.event);
+                console.log(act.time + ": " + act.event);
             }
         }
     }
@@ -169,7 +110,7 @@ function aggregateActions(arr) {
                 excessChanges = (changes - minimumChanges);
                 if (act.parameters.correct == "true") {
                     outcomeStr = "correct";
-                    crystalColor = getCrystalColor(excessChanges, outcomeStr);
+                    crystalColor = getCrystalColor(excessChanges);
                     returnArr.push("[" + act.index + "] Correct drake submitted after " + changes + " changes, " + excessChanges + " of them excess. Crystal color = " + crystalColor);
                     changes = 0;
                 } else {
@@ -187,27 +128,136 @@ function aggregateActions(arr) {
     return returnArr;
 }
 
-function getCrystalColor(excessChanges, outcomeStr) {
-    if ((excessChanges >= 0) && (outcomeStr == "correct")) {
-        if (excessChanges > 2) {
-            return "black";
-        } else {
-            switch (excessChanges) {
-                case 0:
-                    return "blue";
-                case 1:
-                    return "yellow";
-                case 2:
-                    return "red";
-            }
-        }
+function getCrystalColor(thisTry, thisAction) {
+    if (thisAction.parameters.correct == "false") {
+        return 0;
+    } else if (thisTry.excessMoves > 2) {
+        return 1;
     } else {
-        return "no";
+        switch (thisTry.excessMoves) {
+            case 0:
+                return 4;
+            case 1:
+                return 3;
+            case 2:
+                return 2;
+        }
     }
 }
 
-function makeSummary(profile) {
-    for (var i = 0; i < profile.length; i++) {}
+//Report on all the challenges <thisStudent> did. Number of tries, highest crystal achieved.
+function makeProfile(studentId) {
+    var activitiesArray = ["allele-targetMatch-visible-simpleDom", "allele-targetMatch-visible-simpleDom2", "allele-targetMatch-hidden-simpleDom", "allele-targetMatch-hidden-simpleDom2",
+        "allele-targetMatch-visible-armorHorns",
+        "allele-targetMatch-visible-armorHorns2",
+        "allele-targetMatch-visible-armorHorns3",
+        "allele-targetMatch-hidden-armorHorns",
+        "allele-targetMatch-hidden-armorHorns2",
+        "allele-targetMatch-hidden-armorHorns3",
+        "allele-targetMatch-visible-simpleColors",
+        "allele-targetMatch-visible-simpleColors2",
+        "allele-targetMatch-visible-simpleColors3",
+        "allele-targetMatch-visible-simpleColors4",
+        "allele-targetMatch-visible-simpleColors5",
+        "allele-targetMatch-hidden-simpleColors",
+        "allele-targetMatch-hidden-simpleColors2",
+        "allele-targetMatch-hidden-simpleColors3",
+        "allele-targetMatch-visible-harderTraits",
+        "allele-targetMatch-visible-harderTraits2",
+        "allele-targetMatch-hidden-harderTraits",
+        "allele-targetMatch-hidden-harderTraits2"
+    ];
+    var thisActivity,
+        thisStudent = findStudent(studentId),
+        clas = thisStudent.class,
+        tries = [],
+        colorIndexArray = [],
+        thisTry;
+    infoPara.style.display = "block";
+    infoPara.innerHTML = ("Class " + clas.id + ", student " + thisStudent.id + "<br>");
+    for (var j = 0; j < activitiesArray.length; j++) {
+        thisActivity = activitiesArray[j];
+        tries = findTries(studentId, thisActivity);
+        colorIndexArray = [];
+        for (var i = 0; i < tries.length; i++) {
+            thisTry = tries[i];
+            colorIndexArray.push(thisTry.crystalColor);
+        }
+        maxColorIndex = colorIndexArray.reduce(function (a, b) {
+            return Math.max(a, b);
+        });
+        maxColor = getColorFromIndex(maxColorIndex);
+        infoPara.innerHTML += (thisActivity + ": " + tries.length + " tries. Best crystal: " + maxColor + ".<br>")
+    }
+}
+
+
+//A "try" is defined as a sequence of actions starting with a "navigated" event signalling entry into <thisActivity> followed by an "Entered challenge from room" event and ending with either another "navigated" event (signalling getting a new target drake and a new initial drake without submitting a drake) or a "drake submitted" event, which enables us to tag the try with the type of crystal (or none) received. A successful submission triggers a "challenge completed" event and may be followed by a "challenge retried" event, itself followed by a navigation event and a new try for the same challenge.
+function findTries(studentId, thisActivity) {
+    var thisStudent = findStudent(studentId),
+        tries = [],
+        successes = [],
+        failures = [],
+        thisAction,
+        movesForThisDrake,
+        inRemediation = false,
+        ev;
+    for (var i = 0; i < thisStudent.actions.length; i++) {
+        thisAction = thisStudent.actions[i];
+        ev = thisAction.event;
+        if (thisAction.activity == thisActivity) {
+            if (!inRemediation) {
+                if (ev == "Navigated") {
+                    minimumMoves = parseInt(thisAction.parameters.goalMoves);
+                    movesForThisDrake = 0,
+                        thisTry = new Object();
+                    thisTry.moves = 0;
+                    thisTry.hints = 0;
+                    thisTry.remediations = 0;
+                } else if ((ev == "Allele changed") || (ev == "Sex changed")) {
+                    thisTry.moves++;
+                    movesForThisDrake++;
+                } else if (ev == 'Guide hint received') {
+                    thisTry.hints++
+                } else if (ev == 'Started remediation') {
+                    thisTry.remediations++;
+                    inRemediation = true;
+                 
+                //Drake submitted event always ends a try. The drake is either "correct" or "incorrect" (in which case the crystalColor is set to "none"). An incorrect submission necessarily leads to additional moves on the same drake. A correct submission leads to a "Challenge completed" event which then gives the player a choice: "Try Again" or "Continue." "Try Again" produces a "Challenge retried" event, followed by a Navigated (to the same challenge but a different drake). We create a new try but keep the same challenge. "Continue" leads to a Navigated event that points to a different challenge. We create a new try but assign it to a different challenge (if the challenge is among the set we are examining).
+            } else if (ev == 'Drake submitted') {
+                thisTry.excessMoves = (movesForThisDrake - minimumMoves);
+                thisTry.crystalColor = getCrystalColor(thisTry, thisAction);
+                if (thisTry.crystalColor == "none") {
+                    thisTry.moves++;
+                    movesForThisDrake++;
+                }
+                tries.push(thisTry);
+                thisTry = new Object();
+                thisTry.moves = 0;
+                thisTry.hints = 0;
+                thisTry.remediations = 0;
+            }
+            } 
+        } if (ev == 'Ended remediation') {
+            inRemediation = false;
+        }   
+    }
+    return tries;
+}
+
+function getColorFromIndex(colorIndex) {
+    switch (colorIndex) {
+        case 0:
+            return "none";
+        case 1:
+            return ("black");
+        case 2:
+            return ("red");
+        case 3:
+            return ("yellow");
+        case 4:
+            return ("blue");
+    }
 }
 
 function findStudent(id) {
@@ -232,19 +282,4 @@ function findActionsByActivity(studentId, activityName) {
         }
     }
     return returnArr;
-}
-
-function lookForSequence(activityName) {
-    var s,
-        acts
-    act,
-    returnStudents = [];
-    for (let i = 0; i < students.length; i++) {
-        s = students[i];
-        acts = findActionsByActivity(s.id, activityName);
-        for (let j = 0; j < acts.length; j++) {
-            act = acts[j];
-            if (act.event == "Navigated") {}
-        }
-    }
 }
